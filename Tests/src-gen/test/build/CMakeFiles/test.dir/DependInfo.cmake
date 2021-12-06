85a91be8bec363e4d73ba07b8495797578d860cd
LDeng0205 <arthurdeng0205@hotmail.com>
2021-12-05 17:44:34 -0800

    added reactor oriented code for control logic

---
+++ Tests/src-gen/test/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/arthurdeng/Desktop/LFArduino/reactor-c/Tests/src-gen/test/core/platform/lf_macos_support.c" "CMakeFiles/test.dir/core/platform/lf_macos_support.c.o" "gcc" "CMakeFiles/test.dir/core/platform/lf_macos_support.c.o.d"
+  "/Users/arthurdeng/Desktop/LFArduino/reactor-c/Tests/src-gen/test/ctarget.c" "CMakeFiles/test.dir/ctarget.c.o" "gcc" "CMakeFiles/test.dir/ctarget.c.o.d"
+  "/Users/arthurdeng/Desktop/LFArduino/reactor-c/Tests/src-gen/test/test.cpp" "CMakeFiles/test.dir/test.cpp.o" "gcc" "CMakeFiles/test.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Tests/src-gen/test/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/core/platform/lf_macos_support.c.o"
+  "CMakeFiles/test.dir/core/platform/lf_macos_support.c.o.d"
+  "CMakeFiles/test.dir/ctarget.c.o"
+  "CMakeFiles/test.dir/ctarget.c.o.d"
+  "CMakeFiles/test.dir/test.cpp.o"
+  "CMakeFiles/test.dir/test.cpp.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
